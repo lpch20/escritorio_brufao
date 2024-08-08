e35bbdc3f7432018f8842a87d6cf1a947c88fcb0
lpch20 <lpch20@hotmail.com>
2024-08-07 22:20:27 -0300

    add changes

--- src/components/FormContact/FormContact.jsx
+++ src/components/FormContact/FormContact.jsx
@@ -12,24 +12,158 @@
   }
   ```
 */
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import "../ToolTipsRedes/toolTip.css";
 
 export default function FormContact() {
   return (
-    <form id='#contacto'>
-      <div style={{backgroundColor: 'rgb(27, 46, 120)' , borderRadius:'10px', color:'white'}} className="max-w-7xl xl:m-auto p-20">
+    <form>
+      <div
+        style={{
+          backgroundColor: "rgb(27, 46, 120)",
+          borderRadius: "10px",
+          color: "white",
+        }}
+        className="max-w-7xl xl:m-auto p-20"
+      >
         <div className="border-b border-gray-900/10 pb-12 xl:flex xl:justify-between xl:items-center">
-          <h2 style={{fontFamily: "koulen"}} className="font-semibold leading-7 text-5xl xl:text-8xl">Contactate</h2>
-          <img className='w-80 p xl:mt-5 hidden lg:hidden md:hidden xl:flex' src="/Logo_FB.png" alt="" />
+          <h2
+            style={{ fontFamily: "koulen" }}
+            className="font-semibold leading-7 text-5xl xl:text-8xl"
+          >
+            Contactate
+          </h2>
+          <img
+            className="w-80 p xl:mt-5 hidden lg:hidden md:hidden xl:flex"
+            src="/Logo_FB.png"
+            alt=""
+          />
         </div>
 
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 ">Información de contacto</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">El contacto se realizara via mail.</p>
+          <div className="xl:flex xl:justify-between flex flex-col justify-start items-start gap-4">
+            <div>
+              <h2 className="text-base font-semibold leading-7 ">
+              Teléfono: 2400 8389
+              </h2>
+            </div>
+            <div>
+              <ul className="example-nashe mr-0">
+                <li className="icon-content w-4 ">
+                  <a
+                    href="https://linkedin.com/"
+                    aria-label="LinkedIn"
+                    data-social="linkedin"
+                  >
+                    <div className="filled"></div>
+                    <svg
+                      style={{ width: "20px" }}
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-linkedin"
+                      viewBox="0 0 16 16"
+                      xmlSpace="preserve"
+                    >
+                      <path
+                        d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                  </a>
+                  <div className="tooltip">LinkedIn</div>
+                </li>
+                {/* <li className="icon-content w-6">
+          <a
+            href="https://www.github.com/"
+            aria-label="GitHub"
+            data-social="github"
+          >
+            <div className="filled"></div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-github"
+              viewBox="0 0 16 16"
+              xmlSpace="preserve"
+            >
+              <path
+                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </a>
+          <div className="tooltip">GitHub</div>
+        </li> */}
+                <li className="icon-content w-6">
+                  <a
+                    href="https://www.instagram.com/"
+                    aria-label="Whatsapp"
+                    data-social="whatsapp"
+                  >
+                    <div className="filled"></div>
+                    <svg
+                      style={{ width: "20px" }}
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 64 64"
+                      aria-labelledby="title"
+                      aria-describedby="desc"
+                      role="img"
+                    >
+                      <title>WhatsApp</title>
+                      <desc>A color styled icon from Orion Icon Library.</desc>
+                      <path
+                        data-name="layer1"
+                        d="M30.19.031a31.753 31.753 0 0 0-26.735 46.12L.085 62.509A1.235 1.235 0 0 0 1.58 63.96l16.029-3.8A31.744 31.744 0 1 0 30.19.031zM49.316 49.31A24.871 24.871 0 0 1 20.68 54l-2.232-1.112-9.828 2.326 2.069-10.042-1.1-2.154a24.874 24.874 0 0 1 4.578-28.857A24.854 24.854 0 0 1 49.316 49.31zm0 0"
+                        fill="#7ad06d"
+                      ></path>
+                      <path
+                        data-name="layer1"
+                        d="M47.147 38.619L41 36.854a2.292 2.292 0 0 0-2.267.6l-1.5 1.531a2.239 2.239 0 0 1-2.435.514C31.883 38.32 25.765 32.88 24.2 30.16a2.239 2.239 0 0 1 .177-2.483l1.312-1.7a2.292 2.292 0 0 0 .283-2.328L23.388 17.8a2.293 2.293 0 0 0-3.58-.82c-1.716 1.451-3.752 3.657-4 6.1-.436 4.308 1.411 9.738 8.4 16.258 8.071 7.534 14.534 8.528 18.743 7.509 2.387-.578 4.294-2.9 5.5-4.793a2.293 2.293 0 0 0-1.3-3.436z"
+                        fill="#7ad06d"
+                      ></path>
+                    </svg>
+                  </a>
+                  <div className="tooltip">Whatsapp</div>
+                </li>
+                {/* <li className="icon-content w-6">
+          <a
+            href="https://youtube.com/"
+            aria-label="Youtube"
+            data-social="youtube"
+          >
+            <div className="filled"></div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-youtube"
+              viewBox="0 0 16 16"
+              xmlSpace="preserve"
+            >
+              <path
+                d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </a>
+          <div className="tooltip">Youtube</div>
+        </li> */}
+              </ul>
+            </div>
+          </div>
+          {/* <p className="mt-1 text-sm leading-6 text-gray-600">El contacto se realizara via mail.</p> */}
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
-              <label htmlFor="first-name" className="block text-sm font-medium leading-6 ">
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium leading-6 "
+              >
                 Nombre
               </label>
               <div className="mt-2">
@@ -44,7 +178,10 @@ export default function FormContact() {
             </div>
 
             <div className="sm:col-span-3">
-              <label htmlFor="last-name" className="block text-sm font-medium leading-6 ">
+              <label
+                htmlFor="last-name"
+                className="block text-sm font-medium leading-6 "
+              >
                 Apellido
               </label>
               <div className="mt-2">
@@ -59,8 +196,11 @@ export default function FormContact() {
             </div>
 
             <div className="sm:col-span-6">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 ">
-                Email 
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 "
+              >
+                Email
               </label>
               <div className="mt-2">
                 <input
@@ -73,9 +213,11 @@ export default function FormContact() {
               </div>
             </div>
 
-
             <div className="sm:col-span-3 sm:col-start-1">
-              <label htmlFor="city" className="block text-sm font-medium leading-6 ">
+              <label
+                htmlFor="city"
+                className="block text-sm font-medium leading-6 "
+              >
                 Ciudad
               </label>
               <div className="mt-2">
@@ -90,7 +232,10 @@ export default function FormContact() {
             </div>
 
             <div className="sm:col-span-3">
-              <label htmlFor="region" className="block text-sm font-medium leading-6 ">
+              <label
+                htmlFor="region"
+                className="block text-sm font-medium leading-6 "
+              >
                 Numero de Contacto
               </label>
               <div className="mt-2">
@@ -105,8 +250,11 @@ export default function FormContact() {
             </div>
 
             <div className="sm:col-span-6 sm:col-start-1">
-              <label htmlFor="city" className="block text-sm font-medium leading-6 ">
-                Describe aquí porque deseas contactarte 
+              <label
+                htmlFor="city"
+                className="block text-sm font-medium leading-6 "
+              >
+                Describe aquí porque deseas contactarte
               </label>
               <div className="mt-2">
                 <textarea
@@ -122,16 +270,14 @@ export default function FormContact() {
         </div>
 
         <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button
-          type="submit"
-          className="w-full rounded-md lg:w-36 xl:w-36 lg-rounded-md xl-rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Enviar
-        </button>
+          <button
+            type="submit"
+            className="w-full rounded-md lg:w-36 xl:w-36 lg-rounded-md xl-rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Enviar
+          </button>
+        </div>
       </div>
-      </div>
-
-     
     </form>
-  )
+  );
 }
